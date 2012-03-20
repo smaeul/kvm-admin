fc22ceb5410e858fbe7889337f28355ce33588ea
Jens Kasten <jens@kasten-edv.de>
2012-03-20 15:55:45 +0100

    fix import os issue and move search path to header file

--- kvmtools/functions.py
+++ kvmtools/functions.py
@@ -6,16 +6,15 @@
 (c) 2012 jens Kasten <jens@kasten-edv.de>
 """
 
-import sys
+import os
+from header import Header
 
 
 def which(file_name):
     """Python implementation of linux which command."""
     if os.path.isfile(file_name) and os.access(file_name, os.X_OK):
         return file_name 
-    paths = ["/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/sbin",
-        "/usr/local/bin"]
-    for path in paths:
+    for path in Header().search_paths:
         service = os.path.join(path, file_name)
         if os.path.isfile(service) and os.access(service, os.X_OK):
             return service
